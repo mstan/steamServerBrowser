9bb34efea56b596831f07b178b42e04d8e5a8a5c
mattsgm <1379tech@gmail.com>
2015-08-09 17:23:30 -0500

    Add direct server pass from user page

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var addNewServer = require('./lib/addNewServer');
 var getTokenByID = require('./lib/getTokenByID');
 var updateEntryByID = require('./lib/updateEntryByID');
 var deleteEntryByID = require('./lib/deleteEntryByID');
+var connectToServerDirect = require('./lib/connectToServerDirect');
 
 
 /* Start Express */
@@ -25,7 +26,7 @@ var dbFile = './db/db.sqlite';
 var db = new sqlite3.Database(dbFile);
 
 /* middleware */
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
   //For deps to hook reqs
 app.use(function (req,res,next) {
@@ -38,30 +39,43 @@ app.param('id', getTokenByID);
 
 app.set(express.static(__dirname + 'views'));
 app.use(express.static(__dirname + '/bower_components/bootstrap/dist/css'));
+app.use(express.static(__dirname + '/bower_components/jquery/dist/'));
 app.set('view engine', 'ejs');
 
 
 
 /* Routing */
 
+  //Index
 app.get('/', loadAll);
 
 app.post('/', searchFilter);
 
+  //New Server
 app.get('/new', function (req,res) {
-	res.render('pages/addNewServer');
+  res.render('pages/addNewServer');
 })
 
 app.post('/new', addNewServer);
 
+  //Admin View Entry
+
 app.get('/viewEntry/:id', function (req,res) {
     server = req.server;
-    res.render('pages/viewEntry', {server: server});
+    res.render('pages/viewEntryAdmin', {server: server});
 });
 
 app.post('/viewEntry/', updateEntryByID);
 
 app.post('/deleteEntry/', deleteEntryByID);
+
+  //User View Entry
+
+app.get ('/publicServer/:id', function (req,res) {
+    res.render('pages/viewEntryPublic');
+});
+
+app.post ('/connectToServer/', connectToServerDirect);
 
 
 /* Start app at this port */
